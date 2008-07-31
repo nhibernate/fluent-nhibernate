5b5490bf9741762e924c053d8b719a0c6d26c1e7
James Gregory <james@jagregory.com>
2008-07-31 18:02:07 +0000

    Applied identity-generation.patch from Matt Hinze, from Issue 7.
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@13 48f0ce17-cc52-0410-af8c-857c09b6549b

---
+++ src/FluentNHibernate.Testing/DomainModel/Mapping/IdentityPartTester.cs
@@ -0,0 +1,412 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Xml;
+using FluentNHibernate.Mapping;
+using NHibernate.Engine;
+using NHibernate.Id;
+using NHibernate.Util;
+using NUnit.Framework;
+using ShadeTree.Core;
+
+namespace FluentNHibernate.Testing.DomainModel.Mapping
+{
+	[TestFixture]
+	public class IdentityPartTester
+	{
+		[Test, Explicit]
+		public void Scratch()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+			var id = new IdentityPart(property);
+
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			Debug.Write(element.InnerXml);
+		}
+
+
+		[Test]
+		public void Defaults()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property);
+
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			var idElement = (XmlElement)element.SelectSingleNode("id");
+			var generatorElemenent = (XmlElement) idElement.SelectSingleNode("generator");
+			
+			idElement.AttributeShouldEqual("name", "IntId");
+			idElement.AttributeShouldEqual("column", "IntId");
+			idElement.AttributeShouldEqual("type", "Int32");
+			generatorElemenent.AttributeShouldEqual("class", "identity");
+		}
+
+		[Test]
+		public void ColumnName_SpecifyColumnName()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property, "Id");
+
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			var idElement = (XmlElement)element.SelectSingleNode("id");
+
+			idElement.AttributeShouldEqual("column", "Id");
+		}
+
+		[Test]
+		public void GeneratorClass_Long_DefaultsToIdentity()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.LongId);
+			
+			var id = new IdentityPart(property);
+			
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			var idElement = (XmlElement)element.SelectSingleNode("id");
+			var generatorElemenent = (XmlElement)idElement.SelectSingleNode("generator");
+			
+			idElement.AttributeShouldEqual("type", "Int64");
+			generatorElemenent.AttributeShouldEqual("class", "identity");
+		}
+
+		[Test]
+		public void GeneratorClass_Guid_DefaultsToGuidComb()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+
+			var id = new IdentityPart(property);
+
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			var idElement = (XmlElement)element.SelectSingleNode("id");
+			var generatorElemenent = (XmlElement)idElement.SelectSingleNode("generator");
+
+			idElement.AttributeShouldEqual("type", "Guid");
+			generatorElemenent.AttributeShouldEqual("class", "guid.comb");
+		}
+
+		[Test]
+		public void GeneratorClass_String_DefaultsToAssigned()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.StringId);
+
+			var id = new IdentityPart(property);
+
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			var idElement = (XmlElement)element.SelectSingleNode("id");
+			var generatorElemenent = (XmlElement)idElement.SelectSingleNode("generator");
+
+			idElement.AttributeShouldEqual("type", "String");
+			generatorElemenent.AttributeShouldEqual("class", "assigned");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyIncrement()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.Increment();
+			
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "increment");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyIdentity()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.Identity();
+
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "identity");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifySequence()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.Sequence("uid_sequence");
+
+			XmlNodeList paramElements;
+			XmlElement generatorElement = getGeneratorElement(id, out paramElements);
+
+			generatorElement.AttributeShouldEqual("class", "sequence");
+			paramElements.Count.ShouldEqual(1);
+			paramElements[0].Name.ShouldEqual("param");
+			paramElements[0].Attributes["name"].Value.ShouldEqual("sequence");
+			paramElements[0].InnerXml.ShouldEqual("uid_sequence");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyHiLo()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.HiLo("hi_value", "next_value", "100");
+
+			XmlNodeList paramElements;
+			XmlElement generatorElement = getGeneratorElement(id, out paramElements);
+
+			generatorElement.AttributeShouldEqual("class", "hilo");
+			paramElements.Count.ShouldEqual(3);
+			paramElements[0].Name.ShouldEqual("param");
+			paramElements[0].Attributes["name"].Value.ShouldEqual("table");
+			paramElements[0].InnerXml.ShouldEqual("hi_value");
+
+			paramElements[1].Name.ShouldEqual("param");
+			paramElements[1].Attributes["name"].Value.ShouldEqual("column");
+			paramElements[1].InnerXml.ShouldEqual("next_value");
+
+			paramElements[2].Name.ShouldEqual("param");
+			paramElements[2].Attributes["name"].Value.ShouldEqual("max_lo");
+			paramElements[2].InnerXml.ShouldEqual("100");
+
+			var id2 = new IdentityPart(property).GeneratedBy.HiLo("hi_value", "next_value", "100");
+		}
+
+		private XmlElement getGeneratorElement(IdentityPart id, out XmlNodeList paramElements)
+		{
+			var document = new XmlDocument();
+			var element = document.CreateElement("root");
+			id.Write(element, new MappingVisitor());
+			var generatorElement = (XmlElement)element.SelectSingleNode("id/generator");
+			paramElements = generatorElement.SelectNodes("param");
+			return generatorElement;
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifySeqHiLo()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.SeqHiLo("hi_value","100");
+
+			XmlNodeList paramElements;
+			XmlElement generatorElement = getGeneratorElement(id, out paramElements);
+
+			generatorElement.AttributeShouldEqual("class", "seqhilo");
+			paramElements.Count.ShouldEqual(2);
+			paramElements[0].Name.ShouldEqual("param");
+			paramElements[0].Attributes["name"].Value.ShouldEqual("sequence");
+			paramElements[0].InnerXml.ShouldEqual("hi_value");
+
+			paramElements[1].Name.ShouldEqual("param");
+			paramElements[1].Attributes["name"].Value.ShouldEqual("max_lo");
+			paramElements[1].InnerXml.ShouldEqual("100");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyUuidHex()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.StringId);
+
+			var id = new IdentityPart(property).GeneratedBy.UuidHex("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+			XmlNodeList paramElements;
+			XmlElement generatorElement = getGeneratorElement(id, out paramElements);
+
+			generatorElement.AttributeShouldEqual("class", "uuid.hex");
+			paramElements.Count.ShouldEqual(1);
+			paramElements[0].Name.ShouldEqual("param");
+			paramElements[0].Attributes["name"].Value.ShouldEqual("format");
+			paramElements[0].InnerXml.ShouldEqual("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyUuidString()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.StringId);
+
+			var id = new IdentityPart(property).GeneratedBy.UuidString();
+
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "uuid.string");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyGuid()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+
+			var id = new IdentityPart(property).GeneratedBy.Guid();
+
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "guid");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyGuidComb()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+
+			var id = new IdentityPart(property).GeneratedBy.GuidComb();
+
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "guid.comb");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyNative()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.Native();
+
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "native");
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyAssigned()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+
+			var id = new IdentityPart(property).GeneratedBy.Assigned();
+
+			XmlNodeList ignored;
+			XmlElement generatorElement = getGeneratorElement(id, out ignored);
+
+			generatorElement.AttributeShouldEqual("class", "assigned");	
+		}
+
+		[Test]
+		public void GeneratorClass_CanSpecifyForeign()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+
+			var id = new IdentityPart(property).GeneratedBy.Foreign("Parent");
+
+			XmlNodeList paramElements;
+			XmlElement generatorElement = getGeneratorElement(id, out paramElements);
+
+			generatorElement.AttributeShouldEqual("class", "foreign");
+			paramElements.Count.ShouldEqual(1);
+			paramElements[0].Name.ShouldEqual("param");
+			paramElements[0].Attributes["name"].Value.ShouldEqual("property");
+			paramElements[0].InnerXml.ShouldEqual("Parent");
+		}
+
+		[Test]
+		[ExpectedException(typeof (InvalidOperationException))]
+		public void IdentityType_MustBeIntegral_ForIncrement()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+			new IdentityPart(property).GeneratedBy.Increment();
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeIntegral_ForIdentity()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+			new IdentityPart(property).GeneratedBy.Identity();
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeIntegral_ForSequence()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+			new IdentityPart(property).GeneratedBy.Sequence("no");
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeIntegral_ForHiLo()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+			new IdentityPart(property).GeneratedBy.HiLo("no","no","no");
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeIntegral_ForSeqHiLo()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+			new IdentityPart(property).GeneratedBy.SeqHiLo("no", "no");
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeString_ForUuidHex()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+			new IdentityPart(property).GeneratedBy.UuidHex("format");
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeString_ForUuidString()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+			new IdentityPart(property).GeneratedBy.UuidString();
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeGuid_ForGuid()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+			new IdentityPart(property).GeneratedBy.Guid();
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeGuid_ForGuidComb()
+		{
+
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.IntId);
+			new IdentityPart(property).GeneratedBy.GuidComb();
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IdentityType_MustBeIntegral_ForNative()
+		{
+			PropertyInfo property = ReflectionHelper.GetProperty<IdentityTarget>(x => x.GuidId);
+			new IdentityPart(property).GeneratedBy.Native();
+		}
+	}
+
+	public class IdentityTarget
+	{
+		public virtual int IntId { get; set; }
+		public virtual long LongId { get; set; }
+		public virtual Guid GuidId { get; set; }
+		public virtual string StringId { get; set; }
+	}
+}
---
+++ src/FluentNHibernate/Mapping/IdentityGenerationStrategyBuilder.cs
@@ -0,0 +1,217 @@
+using System;
+
+namespace FluentNHibernate.Mapping
+{
+	public class IdentityGenerationStrategyBuilder
+	{
+		private readonly IdentityPart _parent;
+
+		public IdentityGenerationStrategyBuilder(IdentityPart parent)
+		{
+			_parent = parent;
+		}
+
+		private void setGenerator(string generator)
+		{
+			_parent.SetGeneratorClass(generator);
+		}
+
+		private void addGeneratorParam(string name, string innerXml)
+		{
+			_parent.AddGeneratorParam(name, innerXml);
+		}
+
+		private void ensureIntegralIdenityType()
+		{
+			if (!isIntegralType(_parent.IdentityType)) throw new InvalidOperationException("Identity type must be integral (int, long, uint, ulong)");
+		}
+
+		private void ensureGuidIdentityType()
+		{
+			if (_parent.IdentityType != typeof(Guid)) throw new InvalidOperationException("Identity type must be Guid");
+		}
+
+		private void ensureStringIdentityType()
+		{
+			if (_parent.IdentityType != typeof(string)) throw new InvalidOperationException("Identity type must be string");
+		}
+
+		private static bool isIntegralType(Type t)
+		{
+			// do we think we'll encounter more?
+			return t == typeof (int) || t == typeof (long) || t == typeof(uint) || t == typeof(ulong) || t == typeof(byte) || t == typeof(sbyte) || t == typeof(short) || t == typeof(ushort);
+		}
+
+		/// <summary>
+		/// generates identifiers of any integral type that are unique only when no other 
+		/// process is inserting data into the same table. Do not use in a cluster.
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart Increment()
+		{
+			ensureIntegralIdenityType();
+			setGenerator("increment");
+			return _parent;
+		}
+
+		/// <summary>
+		/// supports identity columns in DB2, MySQL, MS SQL Server and Sybase.
+		/// The identifier returned by the database is converted to the property type using 
+		/// Convert.ChangeType. Any integral property type is thus supported.
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart Identity()
+		{
+			ensureIntegralIdenityType();
+			setGenerator("identity");
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses a sequence in DB2, PostgreSQL, Oracle or a generator in Firebird.
+		/// The identifier returned by the database is converted to the property type 
+		/// using Convert.ChangeType. Any integral property type is thus supported.
+		/// </summary>
+		/// <param name="sequenceName"></param>
+		/// <returns></returns>
+		public IdentityPart Sequence(string sequenceName)
+		{
+			ensureIntegralIdenityType();
+			setGenerator("sequence");
+			addGeneratorParam("sequence", sequenceName);
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses a hi/lo algorithm to efficiently generate identifiers of any integral type, 
+		/// given a table and column (by default hibernate_unique_key and next_hi respectively) 
+		/// as a source of hi values. The hi/lo algorithm generates identifiers that are unique 
+		/// only for a particular database. Do not use this generator with a user-supplied connection.
+		/// requires a "special" database table to hold the next available "hi" value
+		/// </summary>
+		/// <param name="table"></param>
+		/// <param name="column"></param>
+		/// <param name="max_lo"></param>
+		/// <returns></returns>
+		public IdentityPart HiLo(string table, string column, string max_lo)
+		{
+			addGeneratorParam("table", table);
+			addGeneratorParam("column", column);
+			return HiLo(max_lo);
+		}
+
+		/// <summary>
+		/// uses a hi/lo algorithm to efficiently generate identifiers of any integral type, 
+		/// given a table and column (by default hibernate_unique_key and next_hi respectively) 
+		/// as a source of hi values. The hi/lo algorithm generates identifiers that are unique 
+		/// only for a particular database. Do not use this generator with a user-supplied connection.
+		/// requires a "special" database table to hold the next available "hi" value
+		/// </summary>
+		/// <param name="max_lo"></param>
+		/// <returns></returns>
+		public IdentityPart HiLo(string max_lo)
+		{
+			ensureIntegralIdenityType();
+			setGenerator("hilo");
+			addGeneratorParam("max_lo", max_lo);
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses an Oracle-style sequence (where supported)
+		/// </summary>
+		/// <param name="sequence"></param>
+		/// <param name="max_lo"></param>
+		/// <returns></returns>
+		public IdentityPart SeqHiLo(string sequence, string max_lo)
+		{
+			ensureIntegralIdenityType();
+			setGenerator("seqhilo");
+			addGeneratorParam("sequence", sequence);
+			addGeneratorParam("max_lo", max_lo);
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses System.Guid and its ToString(string format) method to generate identifiers
+		/// of type string. The length of the string returned depends on the configured format. 
+		/// </summary>
+		/// <param name="format">http://msdn.microsoft.com/en-us/library/97af8hh4.aspx</param>
+		/// <returns></returns>
+		public IdentityPart UuidHex(string format)
+		{
+			ensureStringIdentityType();
+			setGenerator("uuid.hex");
+			addGeneratorParam("format", format);
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses a new System.Guid to create a byte[] that is converted to a string.  
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart UuidString()
+		{
+			ensureStringIdentityType();
+			setGenerator("uuid.string");
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses a new System.Guid as the identifier. 
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart Guid()
+		{
+			ensureGuidIdentityType();
+			setGenerator("guid");
+			return _parent;
+		}
+
+		/// <summary>
+		/// Recommended for Guid identifiers!
+		/// uses the algorithm to generate a new System.Guid described by Jimmy Nilsson 
+		/// in the article http://www.informit.com/articles/article.asp?p=25862. 
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart GuidComb()
+		{
+			ensureGuidIdentityType();
+			setGenerator("guid.comb");
+			return _parent;
+		}
+
+		/// <summary>
+		/// lets the application to assign an identifier to the object before Save() is called. 
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart Assigned()
+		{
+			setGenerator("assigned");
+			return _parent;
+		}
+
+		/// <summary>
+		/// picks identity, sequence or hilo depending upon the capabilities of the underlying database. 
+		/// </summary>
+		/// <returns></returns>
+		public IdentityPart Native()
+		{
+			ensureIntegralIdenityType();
+			setGenerator("native");
+			return _parent;
+		}
+
+		/// <summary>
+		/// uses the identifier of another associated object. Usually used in conjunction with a one-to-one primary key association. 
+		/// </summary>
+		/// <param name="property"></param>
+		/// <returns></returns>
+		public IdentityPart Foreign(string property)
+		{
+			setGenerator("foreign");
+			addGeneratorParam("property", property);
+			return _parent;
+		}
+	}
+}
