d840b4555609b4fc6248d94841f6ee18f88d3c44
Bobby Johnson <bobby.johnson@gmail.com>
2008-08-15 14:54:21 +0000

    Added QuickStart solution contains a single project currently, asking the group what we want for an end to end application.
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@57 48f0ce17-cc52-0410-af8c-857c09b6549b

---
+++ src/QuickStart/Domain/Cat.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentNHibernate.QuickStart.Domain
+{
+    public class Cat
+    {
+        public virtual string Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual char Sex { get; set; }
+
+        public virtual float Weight { get; set; }
+    }
+}
