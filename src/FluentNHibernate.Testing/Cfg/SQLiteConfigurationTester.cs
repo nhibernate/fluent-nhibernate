f18055b700fff870f2b23adf36c20858fa9bfb53
Chad Myers <chad.myers@gmail.com>
2008-08-16 03:10:19 +0000

    Added start of a SQLite configuration
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@63 48f0ce17-cc52-0410-af8c-857c09b6549b

---
+++ src/FluentNHibernate.Testing/Cfg/SQLiteConfigurationTester.cs
@@ -0,0 +1,29 @@
+using FluentNHibernate.Cfg;
+using NUnit.Framework;
+
+namespace FluentNHibernate.Testing.Cfg
+{
+	[TestFixture]
+	public class SQLiteConfigurationTester
+	{
+		[Test]
+		public void should_set_up_default_query_substitutions()
+		{
+			new SQLiteConfiguration().ToProperties()["query.substitutions"].ShouldEqual("true=1;false=0");
+		}
+
+		[Test]
+		public void in_memory_should_set_up_expected_connection_string()
+		{
+			new SQLiteConfiguration().InMemory()
+				.ToProperties()["connection.connection_string"].ShouldEqual("Data Source=:memory:;Version=3;New=True;");
+		}
+
+		[Test]
+		public void using_file_should_set_up_expected_connection_string()
+		{
+			new SQLiteConfiguration().UsingFile("foo")
+				.ToProperties()["connection.connection_string"].ShouldEqual("Data Source=foo;Version=3;New=True;");
+		}
+	}
+}
---
+++ src/FluentNHibernate/Cfg/SQLiteConfiguration.cs
@@ -0,0 +1,25 @@
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace FluentNHibernate.Cfg
+{
+	public class SQLiteConfiguration : PersistenceConfiguration<SQLiteConfiguration>
+	{
+		public SQLiteConfiguration()
+		{
+			Driver<SQLite20Driver>();
+			Dialect<SQLiteDialect>();
+			Raw("query.substitutions", "true=1;false=0");  
+		}
+
+		public SQLiteConfiguration InMemory()
+		{
+			return ConnectionString.Is("Data Source=:memory:;Version=3;New=True;");
+		}
+
+		public SQLiteConfiguration UsingFile(string fileName)
+		{
+			return ConnectionString.Is(string.Format("Data Source={0};Version=3;New=True;", fileName));
+		}
+	}
+}
