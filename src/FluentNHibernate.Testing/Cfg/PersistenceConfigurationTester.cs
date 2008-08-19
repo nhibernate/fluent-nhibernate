0c7d201b49ab63fa753bf4de1f88ec60a2a74d06
Chad Myers <chad.myers@gmail.com>
2008-08-19 04:34:28 +0000

    Patch submitted by Zach Young - all but one of the documented NHib config properties implemented
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@72 48f0ce17-cc52-0410-af8c-857c09b6549b

--- src/FluentNHibernate.Testing/Cfg/PersistenceConfigurationTester.cs
+++ src/FluentNHibernate.Testing/Cfg/PersistenceConfigurationTester.cs
@@ -47,34 +47,83 @@ namespace FluentNHibernate.Testing.Cfg
 			ValueOf("hibernate.dialect").ShouldEqual("foo");
 		}
 
-		[Test]
+	    [Test]
+	    public void Default_Schema_should_be_set_to_schema_name()
+	    {
+	         _config.DefaultSchema("foo");
+            ValueOf("default_schema").ShouldEqual("foo");
+	    }
+
+	    [Test]
+	    public void UseOuterJoin_should_set_value_to_const_true()
+	    {
+	        _config.UseOuterJoin();
+	        ValueOf("use_outer_join").ShouldEqual("true");
+	    }
+
+	    [Test]
+	    public void Max_Fetch_Depth_should_set_property_value()
+	    {
+	        _config.MaxFetchDepth(2);
+	        ValueOf("max_fetch_depth").ShouldEqual("2");
+	    }
+
+	    [Test]
+	    public void Use_Reflection_Optimizer_should_set_value_to_const_true()
+	    {
+	        _config.UseReflectionOptimizer();
+	        ValueOf("use_reflection_optimizer").ShouldEqual("true");
+
+	    }
+
+	    [Test]
+        public void Provider_Class_should_set_property_value()
+	    {
+			_config.Cache.ProviderClass("foo");
+            ValueOf("cache.provider_class").ShouldEqual("foo");
+
+	    }
+
+	    [Test]
+        public void Use_Minimal_Puts_should_set_value_to_const_true()
+	    {
+			_config.Cache.UseMininmalPuts();
+            ValueOf("cache.use_minimal_puts").ShouldEqual("true");
+	    }
+
+	    [Test]
+        public void Use_Query_Cache_should_set_value_to_const_true()
+	    {
+			_config.Cache.UseQueryCache();
+            ValueOf("cache.use_query_cache").ShouldEqual("true");
+	    }
+
+	    [Test]
+        public void Query_Cache_Factory_should_set_property_value()
+	    {
+			_config.Cache.QueryCacheFactory("foo");
+            ValueOf("cache.query_cache_factory").ShouldEqual("foo");
+	    }
+
+	    [Test]
+        public void Region_Prefix_should_set_property_value()
+	    {
+	        _config.Cache.RegionPrefix("foo");
+            ValueOf("cache.region_prefix").ShouldEqual("foo");
+	    }
+
+	    [Test]
+        public void Query_Substitutions_should_set_property_value()
+	    {
+	        _config.QuerySubstitutions("foo");
+            ValueOf("query.substitutions").ShouldEqual("foo");
+	    }
+	    [Test]
 		public void Show_Sql_should_set_value_to_const_true()
 		{
 			_config.ShowSql();
 			ValueOf("show_sql").ShouldEqual("true");
 		}
-
-		[Test]
-		public void UseOuterJoin_should_set_value_to_const_true()
-		{
-			_config.UseOuterJoin();
-			ValueOf("use_outer_join").ShouldEqual("true");
-		}
-
-        [Test]
-        public void Use_Reflection_Optimizer_should_set_value_to_const_true()
-        {
-            _config.UseReflectionOptimizer();
-            ValueOf("use_reflection_optimizer").ShouldEqual("true");
-
-        }
-
-	    [Test]
-        public void Max_Fetch_Depth_should_set_property_value()
-	    {
-	        _config.MaxFetchDepth(2);
-            ValueOf("max_fetch_depth").ShouldEqual("2");
-	    }
 
 	    [Test]
 	    public void DoNot_ShowSql_should_set_the_property_to_const_false()
--- src/FluentNHibernate/Cfg/PersistenceConfiguration.cs
+++ src/FluentNHibernate/Cfg/PersistenceConfiguration.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Configuration;
+using NHibernate.Cache;
 using NHibernate.Connection;
 using NHibernate.Dialect;
 using NHibernate.Driver;
@@ -17,109 +18,134 @@ namespace FluentNHibernate.Cfg
 	{
 		protected const string DialectKey = "dialect"; // Newer one, but not supported by everything
 		protected const string AltDialectKey = "hibernate.dialect"; // Some older NHib tools require this
-		protected const string ShowSqlKey = "show_sql";
+        protected const string DefaultSchemaKey = "default_schema"; 
+        protected const string UseOuterJoinKey = "use_outer_join";
+        protected const string MaxFetchDepthKey = "max_fetch_depth";
+        protected const string UseReflectionOptimizerKey = "use_reflection_optimizer";
+        protected const string QuerySubstitutionsKey = "query.substitutions";
+        protected const string ShowSqlKey = "show_sql";
+
 		protected const string ConnectionProviderKey = "connection.provider";
 		protected const string DefaultConnectionProviderClassName = "NHibernate.Connection.DriverConnectionProvider";
 		protected const string DriverClassKey = "connection.driver_class";
 		protected const string ConnectionStringKey = "connection.connection_string";
-		protected const string UseOuterJoinKey = "use_outer_join";
-        protected const string UseReflectionOptimizerKey = "use_reflection_optimizer";
-        protected const string MaxFetchDepthKey = "max_fetch_depth";
 
-		private readonly Dictionary<string, string> _rawValues;
-		private readonly Cache<string, string> _values;
+	    private readonly Dictionary<string, string> _rawValues;
+
+	    private readonly Cache<string, string> _values;
+
 	    private bool _nextBoolSettingValue = true;
 
-		protected PersistenceConfiguration()
+	    protected PersistenceConfiguration()
 		{
 			_rawValues = new Dictionary<string, string>();
 			_values = new Cache<string, string>(_rawValues, s=>"");
 			_values.Store(ConnectionProviderKey, DefaultConnectionProviderClassName);
 		}
 
-		public NHibConfiguration ConfigureProperties(NHibConfiguration nhibernateConfig)
+	    public NHibConfiguration ConfigureProperties(NHibConfiguration nhibernateConfig)
 		{
 			nhibernateConfig.SetProperties(_rawValues);
 			return nhibernateConfig;
 		}
 
-		public IDictionary<string, string> ToProperties()
+	    public IDictionary<string, string> ToProperties()
 		{
 			return new Dictionary<string, string>(_rawValues);
 		}
 
-		public THIS Dialect(string dialect)
+        protected void toggleBooleanSetting(string settingKey)
+        {
+            var value = _nextBoolSettingValue.ToString().ToLowerInvariant();
+
+            _values.Store(settingKey, value);
+
+            _nextBoolSettingValue = true;
+        }
+
+        public THIS DoNot
+        {
+            get
+            {
+                _nextBoolSettingValue = false;
+                return (THIS)this;
+            }
+        }
+
+	    public THIS Dialect(string dialect)
 		{
 			_values.Store(DialectKey, dialect);
 			_values.Store(AltDialectKey, dialect);
 			return (THIS) this;
 		}
 
-		public THIS Dialect<T>()
+	    public THIS Dialect<T>()
 			where T : Dialect
 		{
 			return Dialect(typeof (T).AssemblyQualifiedName);
 		}
 
-		public THIS Provider(string provider)
-		{
-			_values.Store(ConnectionProviderKey, provider);
-			return (THIS) this;
-		}
-
-		public THIS Provider<T>()
-			where T : IConnectionProvider
-		{
-			return Provider(typeof (T).AssemblyQualifiedName);
-		}
-
-		public THIS Driver(string driverClass)
-		{
-			_values.Store(DriverClassKey, driverClass);
-			return (THIS) this;
-		}
-
-		public THIS Driver<T>()
-			where T : IDriver
-		{
-			return Driver(typeof (T).AssemblyQualifiedName);
-		}
-
-		public THIS ShowSql()
-		{
-            toggleBooleanSetting(ShowSqlKey);
-			return (THIS) this;
-		}
+	    public THIS DefaultSchema(string schema)
+	    {
+            _values.Store(DefaultSchemaKey, schema);
+            return (THIS) this;
+	    }
 
 	    public THIS UseOuterJoin()
 		{
             toggleBooleanSetting(UseOuterJoinKey);
-			return (THIS)this;
+			return (THIS) this;
 		}
+
+        public THIS MaxFetchDepth(int maxFetchDepth)
+        {
+            _values.Store(MaxFetchDepthKey, maxFetchDepth.ToString());
+            return (THIS)this;
+        }
 
 	    public THIS UseReflectionOptimizer()
 		{
             toggleBooleanSetting(UseReflectionOptimizerKey);
-			return (THIS)this;
+			return (THIS) this;
 		}
 
-	    protected void toggleBooleanSetting(string settingKey)
+  
+	    public THIS QuerySubstitutions(string query)
 	    {
-	        var value = _nextBoolSettingValue.ToString().ToLowerInvariant();
-
-	        _values.Store(settingKey, value);
-
-	        _nextBoolSettingValue = true;
+            _values.Store(QuerySubstitutionsKey, query);
+            return (THIS)this;
 	    }
 
-	    public THIS DoNot
-	    {
-	        get
-	        {
-	            _nextBoolSettingValue = false;
-	            return (THIS)this;
-	        }
-	    }
+	    public THIS ShowSql()
+        {
+            toggleBooleanSetting(ShowSqlKey);
+            return (THIS)this;
+        }
+
+	    public THIS Provider(string provider)
+        {
+            _values.Store(ConnectionProviderKey, provider);
+            return (THIS)this;
+        }
+
+	    public THIS Provider<T>()
+            where T : IConnectionProvider
+        {
+            return Provider(typeof(T).AssemblyQualifiedName);
+        }
+
+	    public THIS Driver(string driverClass)
+        {
+            _values.Store(DriverClassKey, driverClass);
+            return (THIS)this;
+        }
+
+	    public THIS Driver<T>()
+            where T : IDriver
+        {
+            return Driver(typeof(T).AssemblyQualifiedName);
+        }
+
 
 	    public ConnectionStringExpression<THIS> ConnectionString
 		{
@@ -129,17 +155,20 @@ namespace FluentNHibernate.Cfg
 			}
 		}
 
+		public CacheSettingsExpression<THIS> Cache
+		{
+			get
+			{
+				return new CacheSettingsExpression<THIS>((THIS) this);
+			}
+		}
+
 	    public THIS Raw(string key, string value)
 		{
 			_values.Store(key, value);
 			return (THIS) this;
 		}
 
-	    public THIS MaxFetchDepth(int maxFetchDepth)
-	    {
-	        _values.Store(MaxFetchDepthKey, maxFetchDepth.ToString());
-            return (THIS)this;
-	    }
 
 	    public class ConnectionStringExpression<CONFIG>
 			where CONFIG : PersistenceConfiguration<CONFIG>
@@ -168,6 +197,65 @@ namespace FluentNHibernate.Cfg
 			public CONFIG Is(string rawConnectionString)
 			{
 				return _config.Raw(ConnectionStringKey, rawConnectionString);
+			}
+		}
+
+		public class CacheSettingsExpression<CONFIG>
+			where CONFIG : PersistenceConfiguration<CONFIG>
+		{
+			protected const string ProviderClassKey = "cache.provider_class";
+			protected const string CacheUseMininmalPutsKey = "cache.use_minimal_puts";
+			protected const string CacheUseQueryCacheKey = "cache.use_query_cache";
+			protected const string CacheQueryCacheFactoryKey = "cache.query_cache_factory";
+			protected const string CacheRegionPrefixKey = "cache.region_prefix";
+
+			private readonly CONFIG _config;
+
+			public CacheSettingsExpression(CONFIG config)
+			{
+				_config = config;
+			}
+
+			public CONFIG ProviderClass(string providerclass)
+			{
+				_config.Raw(ProviderClassKey, providerclass);
+				return _config;
+			}
+
+			public CONFIG ProviderClass<T>()
+				where T : ICacheProvider
+			{
+				return ProviderClass(typeof(T).AssemblyQualifiedName);
+			}
+
+			public CONFIG UseMininmalPuts()
+			{
+				_config.toggleBooleanSetting(CacheUseMininmalPutsKey);
+				return _config;
+			}
+
+			public CONFIG UseQueryCache()
+			{
+				_config.toggleBooleanSetting(CacheUseQueryCacheKey);
+				return _config;
+			}
+
+			public CONFIG QueryCacheFactory(string factory)
+			{
+				_config.Raw(CacheQueryCacheFactoryKey, factory);
+				return _config;
+			}
+
+			public CONFIG QueryCacheFactory<T>()
+				where T : IQueryCacheFactory
+			{
+				return QueryCacheFactory(typeof(T).AssemblyQualifiedName);
+			}
+
+			public CONFIG RegionPrefix(string prefix)
+			{
+				_config.Raw(CacheRegionPrefixKey, prefix);
+				return _config;
 			}
 		}
 	}
