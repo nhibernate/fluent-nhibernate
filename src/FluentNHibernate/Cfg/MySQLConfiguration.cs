215e36beb00099169cb0537536eed5b060429fa0
Paul Batum <paul.batum@gmail.com>
2008-12-12 04:56:27 +0000

    Applied patch for MySQL support submitted by m4bwav.
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@149 48f0ce17-cc52-0410-af8c-857c09b6549b

---
+++ src/FluentNHibernate/Cfg/MySQLConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace FluentNHibernate.Cfg
+{
+    public class MySQLConfiguration : PersistenceConfiguration<MySQLConfiguration>
+    {
+        protected MySQLConfiguration()
+        {
+            Driver<MySqlDataDriver>();
+        }
+
+        public static MySQLConfiguration Standard
+        {
+            get
+            {
+                return new MySQLConfiguration().Dialect<MySQLDialect>();
+            }
+        }
+    }
+}
+
