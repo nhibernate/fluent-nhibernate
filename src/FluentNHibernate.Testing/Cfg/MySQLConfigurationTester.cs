a98bcaebcf2f900f745cf3af9736b3856488a41d
Chad Myers <chad.myers@gmail.com>
2009-01-02 03:35:05 +0000

    Patch from m4bwav for MySQL ConnectionString configuration + test
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@176 48f0ce17-cc52-0410-af8c-857c09b6549b

---
+++ src/FluentNHibernate.Testing/Cfg/MySQLConfigurationTester.cs
@@ -0,0 +1,35 @@
+using FluentNHibernate.Cfg;
+using NHibernate;
+using NUnit.Framework;
+
+namespace FluentNHibernate.Testing.Cfg
+{
+    [TestFixture]
+    public class MySQLConfigurationTester
+    {
+        [Test]
+        public void MySQL_should_default_to_the_MySQL_dialect()
+        {
+            MySQLConfiguration.Standard.ToProperties()["dialect"].ShouldEqual("NHibernate.Dialect.MySQLDialect, " + typeof(ISession).Assembly.FullName);
+        }
+
+        [Test]
+        public void MySQL_driver_should_default_to_the_MySQL_ClientDriver()
+        {
+            MySQLConfiguration.Standard.ToProperties()["connection.driver_class"].ShouldEqual("NHibernate.Driver.MySqlDataDriver, " + typeof(ISession).Assembly.FullName);
+        }
+
+        [Test]
+        public void ConnectionString_is_added_to_the_configuration()
+        {
+            MySQLConfiguration.Standard
+                .ConnectionString
+                    .Server("db-srv")
+                    .Database("tables")
+                    .Username("toni tester")
+                    .Password("secret")
+                    .Create
+                .ToProperties()["connection.connection_string"].ShouldEqual("Server=db-srv;Database=tables;User ID=toni tester;Password=secret");
+        }
+    }
+}
--- src/FluentNHibernate/Cfg/MySQLConfiguration.cs
+++ src/FluentNHibernate/Cfg/MySQLConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using NHibernate.Dialect;
+﻿using System.Text;
+using NHibernate.Dialect;
 using NHibernate.Driver;
 
 namespace FluentNHibernate.Cfg
@@ -15,6 +16,63 @@ namespace FluentNHibernate.Cfg
             get
             {
                 return new MySQLConfiguration().Dialect<MySQLDialect>();
+            }
+        }
+
+        public new MySQLConnectionStringExpression ConnectionString
+        {
+            get
+            {
+                return new MySQLConnectionStringExpression(this);
+            }
+        }
+
+        public class MySQLConnectionStringExpression : ConnectionStringExpression<MySQLConfiguration>
+        {
+            private string _server;
+            private string _database;
+            private string _username;
+            private string _password;
+
+            public MySQLConnectionStringExpression(MySQLConfiguration config)
+                : base(config)
+            {
+            }
+
+            public MySQLConnectionStringExpression Server(string server)
+            {
+                _server = server;
+                return this;
+            }
+
+            public MySQLConnectionStringExpression Database(string database)
+            {
+                _database = database;
+                return this;
+            }
+
+            public MySQLConnectionStringExpression Username(string username)
+            {
+                _username = username;
+                return this;
+            }
+
+            public MySQLConnectionStringExpression Password(string password)
+            {
+                _password = password;
+                return this;
+            }
+
+            public MySQLConfiguration Create
+            {
+                get
+                {
+                    var sb = new StringBuilder();
+
+                    sb.AppendFormat("Server={0};Database={1};User ID={2};Password={3}", _server, _database, _username, _password);
+
+                    return _config.Raw(ConnectionStringKey, sb.ToString());
+                }
             }
         }
     }
