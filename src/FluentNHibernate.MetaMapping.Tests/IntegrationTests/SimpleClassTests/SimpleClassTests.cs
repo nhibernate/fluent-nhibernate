02a40f87df9e394d417f6eb26899f93ed7b7c92b
Tuna Toksoz <tehlike@gmail.com>
2009-01-31 18:26:49 +0000

    Initial work on meta mapping.
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/branches/pb-rewrite@251 48f0ce17-cc52-0410-af8c-857c09b6549b

---
+++ src/FluentNHibernate.MetaMapping.Tests/IntegrationTests/SimpleClassTests/SimpleClassTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.MappingModel.Conventions;
+using FluentNHibernate.MetaMapping.Tests.IntegrationTests.SimpleClassTests.Domain;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace FluentNHibernate.MetaMapping.Tests.IntegrationTests.SimpleClassTests
+{
+	[TestFixture]
+	public class SimpleClassTests
+	{
+		[SetUp]
+		public void SetUp()
+		{
+			var cfg = MsSqlConfiguration.MsSql2005
+				.ShowSql()
+				.ConnectionString.Is("Server=(local);initial catalog=nhibernate;Integrated Security=SSPI")
+				.ShowSql()
+				.ConfigureProperties(new Configuration());
+			var mappings = cfg.CreateMappings();
+			var dialect = new SQLiteDialect();
+			var model = new PersistenceModel();
+			model.Add(new PersonMap());
+			model.AddConvention(new NamingConvention());
+			var hibernateMapping = model.BuildHibernateMapping();
+			var classBinder = new RootClassBinder(mappings, dialect);
+			foreach (var mapping in hibernateMapping.Classes)
+			{
+				classBinder.Bind(mapping);
+			}
+			cfg.BuildMapping();
+			schemaExport = new SchemaExport(cfg);
+
+			
+		}
+		[TearDown]
+		public void TearDown()
+		{
+
+		}
+		private  SchemaExport schemaExport;
+
+
+
+		[Test]
+		public void Class_with_no_relation_should_be_bound()
+		{
+			schemaExport.Execute(true, true, false, true);
+			schemaExport.Execute(true, true, true, true);
+		}
+
+
+	}
+}
---
+++ src/FluentNHibernate.MetaMapping/Helpers/StringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentNHibernate.MetaMapping.Helpers
+{
+	public static class StrHelper
+	{
+		public static bool IsNullOrEmpty(this string thiz)
+		{
+			return string.IsNullOrEmpty(thiz);
+		}
+		public static string ValueOrDefault(this string thiz,string @default)
+		{
+			return thiz.IsNullOrEmpty() ? @default : thiz;
+		}
+	}
+}
