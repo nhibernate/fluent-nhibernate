acd15a9fbc9510ba414167537de8ecc9d59c3958
Paul Batum <paul.batum@gmail.com>
2009-03-21 05:00:22 +0000

    Added some tests for HbmBagMappingWriter
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/branches/pb-rewrite@420 48f0ce17-cc52-0410-af8c-857c09b6549b

--- src/FluentNHibernate.Testing/MappingModel/Output/HbmBagWriterTester.cs
+++ src/FluentNHibernate.Testing/MappingModel/Output/HbmBagWriterTester.cs
@@ -17,18 +17,17 @@ namespace FluentNHibernate.Testing.MappingModel.Output
 
             var contentsWriter = MockRepository.GenerateStub<IHbmWriter<ICollectionContentsMapping>>();
             contentsWriter.Expect(x => x.Write(bag.Contents)).Return(new HbmOneToMany { @class = "class1" });
+
             var keyWriter = MockRepository.GenerateStub<IHbmWriter<KeyMapping>>();
             keyWriter.Expect(x => x.Write(bag.Key)).Return(new HbmKey());
 
             var writer = new HbmBagWriter(contentsWriter, keyWriter);
-
             writer.ShouldGenerateValidOutput(bag);
         }
 
         [Test]
         public void Should_write_the_attributes()
         {
-
             var testHelper = new HbmTestHelper<BagMapping>();
             testHelper.Check(x => x.Name, "bag1").MapsToAttribute("name");
             testHelper.Check(x => x.OrderBy, "column1").MapsToAttribute("order-by");
@@ -37,6 +36,36 @@ namespace FluentNHibernate.Testing.MappingModel.Output
 
             var writer = new HbmBagWriter(null, null);
             testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void Should_write_the_contents()
+        {
+            var bag = new BagMapping {Contents = new OneToManyMapping()};
+
+            var contentsWriter = MockRepository.GenerateMock<IHbmWriter<ICollectionContentsMapping>>();
+            contentsWriter.Expect(x => x.Write(bag.Contents))
+                .Return(new HbmOneToMany());
+
+            var writer = new HbmBagWriter(contentsWriter, null);
+
+            writer.VerifyXml(bag)
+                .Element("one-to-many").Exists();
+        }
+
+        [Test]
+        public void Should_write_the_key()
+        {
+            var bag = new BagMapping { Key = new KeyMapping()};
+
+            var keyWriter = MockRepository.GenerateMock<IHbmWriter<KeyMapping>>();
+            keyWriter.Expect(x => x.Write(bag.Key))
+                .Return(new HbmKey());
+
+            var writer = new HbmBagWriter(null, keyWriter);
+
+            writer.VerifyXml(bag)
+                .Element("key").Exists();
         }
 
 
