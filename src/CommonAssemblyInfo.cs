44c437983c43bc882fc8c434fb154db616ea5980
Hudson Akridge <Hudson.Akridge@gmail.com>
2009-05-12 19:53:39 +0000

    Test for Jame's Github mirror
    git-svn-id: https://fluent-nhibernate.googlecode.com/svn/trunk@496 48f0ce17-cc52-0410-af8c-857c09b6549b

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Security;
 
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyCopyright("Copyright 2008-2009 James Gregory and contributors (Paul Batum, Andrew Stewart, Chad Myers et al). All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright 2008-2009 James Gregory and contributors (Paul Batum, Andrew Stewart, Chad Myers, Hudson Akridge et al). All rights reserved.")]
 [assembly: AssemblyProduct("Fluent NHibernate")]
 [assembly: AssemblyCompany("http://fluentnhibernate.org")]
 [assembly: AssemblyConfiguration("debug")]
