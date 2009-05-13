b8e246f32455b29cf324b3daadb215c7eaac3726
James Gregory <james@jagregory.com>
2009-05-13 14:24:46 +0100

    Removed Repository implementations and dependency on NHibernate.Linq

--- src/FluentNHibernate.Testing/DomainModel/ConnectedTester.cs
+++ src/FluentNHibernate.Testing/DomainModel/ConnectedTester.cs
@@ -1,9 +1,7 @@
-﻿using System.Diagnostics;
-using FluentNHibernate.Cfg.Db;
+﻿using FluentNHibernate.Cfg.Db;
 using FluentNHibernate.Data;
 using FluentNHibernate.Mapping;
 using FluentNHibernate.Testing.Fixtures;
-using NHibernate;
 using NUnit.Framework;
 
 namespace FluentNHibernate.Testing.DomainModel
@@ -33,109 +31,6 @@ namespace FluentNHibernate.Testing.DomainModel
 
             _source = new SingleConnectionSessionSourceForSQLiteInMemoryTesting(properties, new TestPersistenceModel());
             _source.BuildSchema();
-        }
-
-        [Test]
-        public void Spin_up_the_Linq_stuff()
-        {
-            ISession session = _source.CreateSession();
-
-            session.SaveOrUpdate(new Record{Name = "Jeremy", Age = 34});
-            session.SaveOrUpdate(new Record{Name = "Jessica", Age = 29});
-            session.SaveOrUpdate(new Record{Name = "Natalie", Age = 25});
-            session.SaveOrUpdate(new Record{Name = "Hank", Age = 29});
-            session.SaveOrUpdate(new Record{Name = "Darrell", Age = 34});
-            session.SaveOrUpdate(new Record{Name = "Bill", Age = 34});
-            session.SaveOrUpdate(new Record{Name = "Tim", Age = 35});
-            session.SaveOrUpdate(new Record{Name = "Greg", Age = 36});
-
-            //ISession session2 = _source.CreateSession();
-            //var query = from record in session2.Linq<Record>() where record.Age < 30 select record;
-            //foreach (Record record in query.ToList())
-            //{
-            //    Debug.WriteLine(record.Name);
-            //}
-
-            //return;
-
-            Repository repository = new Repository(_source.CreateSession());
-            Record[] records = repository.Query<Record>(record => record.Age == 29);
-            //records.Length.ShouldEqual(2);
-
-            foreach (var record in records)
-            {
-                Debug.WriteLine(record.Name);
-            }
-        }
-
-        [Test]
-        public void QueryBy_test()
-        {
-            ISession session = _source.CreateSession();
-
-            session.SaveOrUpdate(new Record { Name = "Jeremy", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Jessica", Age = 29 });
-            session.SaveOrUpdate(new Record { Name = "Natalie", Age = 25 });
-            session.SaveOrUpdate(new Record { Name = "Hank", Age = 29 });
-            session.SaveOrUpdate(new Record { Name = "Darrell", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Bill", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Tim", Age = 35 });
-            session.SaveOrUpdate(new Record { Name = "Greg", Age = 36 });
-
-            Repository repository = new Repository(_source.CreateSession());
-            Record record = repository.FindBy<Record, string>(r => r.Name, "Hank");
-            record.Name.ShouldEqual("Hank");
-        }
-
-        [Test]
-        public void Query_by_filters()
-        {
-            ISession session = _source.CreateSession();
-
-            session.SaveOrUpdate(new Record { Name = "Jeremy", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Jeremy", Age = 35 });
-            session.SaveOrUpdate(new Record { Name = "Jessica", Age = 29 });
-            session.SaveOrUpdate(new Record { Name = "Natalie", Age = 25 });
-            session.SaveOrUpdate(new Record { Name = "Hank", Age = 29 });
-            session.SaveOrUpdate(new Record { Name = "Darrell", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Bill", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Tim", Age = 35 });
-            session.SaveOrUpdate(new Record { Name = "Greg", Age = 36 });
-
-            Repository repository = new Repository(_source.CreateSession());
-            Record record = repository.FindBy<Record>(r => r.Age == 34 && r.Name == "Jeremy");
-            record.Name.ShouldEqual("Jeremy");
-            record.Age.ShouldEqual(34);
-        }
-
-        [Test]
-        public void Query_with_multiple_expressions()
-        {
-            ISession session = _source.CreateSession();
-
-            session.SaveOrUpdate(new Record { Name = "Jeremy", Age = 34 });
-            session.SaveOrUpdate(new Record { Name = "Jeremy", Age = 35 });
-
-            Repository repository = new Repository(_source.CreateSession());
-            Record[] records = repository.Query<Record>(r => r.Age >= 35 && r.Name == "Jeremy");
-
-            records.ShouldHaveCount(1);
-            records[0].Name.ShouldEqual("Jeremy");
-            records[0].Age.ShouldEqual(35);
-        }
-
-        [Test]
-        public void Save_and_find()
-        {
-            Repository repository1 = new Repository(_source.CreateSession());
-            var record1 = new Record { Name = "Jeremy", Age = 34 };
-            repository1.Save(record1);
-
-            Repository repository2 = new Repository(_source.CreateSession());
-            Record record2 = repository2.Find<Record>(record1.Id);
-
-            record1.ShouldNotBeTheSameAs(record2);
-            record2.Name.ShouldEqual("Jeremy");
         }
 
         [Test]
