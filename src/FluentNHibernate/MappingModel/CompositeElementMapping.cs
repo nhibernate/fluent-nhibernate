b9f5789f5f24fbac6f4cdb157eac516795000580
Hudson Akridge <Hudson.Akridge@gmail.com>
2009-05-13 21:19:02 -0500

    Started CompositeElementMapping work

---
+++ src/FluentNHibernate/MappingModel/CompositeElementMapping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentNHibernate.MappingModel
+{
+    public class CompositeElementMapping : MappingBase
+    {
+        public override void AcceptVisitor(IMappingModelVisitor visitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
