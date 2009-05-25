5af6b241eff64c938f018fe447c9fa34a75cdef5
James Gregory <james@jagregory.com>
2009-05-25 09:49:37 +0100

    Finished many-to-one model and writer

--- src/FluentNHibernate.Testing/MappingModel/Output/XmlManyToOneWriterTester.cs
+++ src/FluentNHibernate.Testing/MappingModel/Output/XmlManyToOneWriterTester.cs
@@ -19,5 +19,116 @@ namespace FluentNHibernate.Testing.MappingModel.Output
 
             testHelper.VerifyAll(writer);
         }
+
+        [Test]
+        public void ShouldWriteCascadeAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Cascade, "all").MapsToAttribute("cascade");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteClassAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Class, "type").MapsToAttribute("class");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteFetchAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Fetch, "select").MapsToAttribute("fetch");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteForeignKeyAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.ForeignKey, "fk").MapsToAttribute("foreign-key");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteInsertAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Insert, true).MapsToAttribute("insert");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteLazyAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Lazy, true).MapsToAttribute("lazy");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteNameAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Name, "nm").MapsToAttribute("name");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteNotFoundAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.NotFound, "nf").MapsToAttribute("not-found");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteOuterJoinAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.OuterJoin, "outer-join").MapsToAttribute("outer-join");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWritePropertyRefAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.PropertyRef, "pr").MapsToAttribute("property-ref");
+
+            testHelper.VerifyAll(writer);
+        }
+
+        [Test]
+        public void ShouldWriteUpdateAttribute()
+        {
+            writer = new XmlManyToOneWriter();
+            var testHelper = new XmlWriterTestHelper<ManyToOneMapping>();
+            testHelper.Check(x => x.Update, true).MapsToAttribute("update");
+
+            testHelper.VerifyAll(writer);
+        }
+
     }
 }
--- src/FluentNHibernate/MappingModel/Output/XmlManyToOneWriter.cs
+++ src/FluentNHibernate/MappingModel/Output/XmlManyToOneWriter.cs
@@ -19,6 +19,42 @@ namespace FluentNHibernate.MappingModel.Output
             document = new XmlDocument();
 
             var element = document.AddElement("many-to-one");
+
+            if (mapping.Attributes.IsSpecified(x => x.Access))
+                element.WithAtt("access", mapping.Access);
+
+            if (mapping.Attributes.IsSpecified(x => x.Cascade))
+                element.WithAtt("cascade", mapping.Cascade);
+
+            if (mapping.Attributes.IsSpecified(x => x.Class))
+                element.WithAtt("class", mapping.Class);
+
+            if (mapping.Attributes.IsSpecified(x => x.Fetch))
+                element.WithAtt("fetch", mapping.Fetch);
+
+            if (mapping.Attributes.IsSpecified(x => x.ForeignKey))
+                element.WithAtt("foreign-key", mapping.ForeignKey);
+
+            if (mapping.Attributes.IsSpecified(x => x.Insert))
+                element.WithAtt("insert", mapping.Insert);
+
+            if (mapping.Attributes.IsSpecified(x => x.Lazy))
+                element.WithAtt("lazy", mapping.Lazy);
+
+            if (mapping.Attributes.IsSpecified(x => x.Name))
+                element.WithAtt("name", mapping.Name);
+
+            if (mapping.Attributes.IsSpecified(x => x.NotFound))
+                element.WithAtt("not-found", mapping.NotFound);
+
+            if (mapping.Attributes.IsSpecified(x => x.OuterJoin))
+                element.WithAtt("outer-join", mapping.OuterJoin);
+
+            if (mapping.Attributes.IsSpecified(x => x.PropertyRef))
+                element.WithAtt("property-ref", mapping.PropertyRef);
+
+            if (mapping.Attributes.IsSpecified(x => x.Update))
+                element.WithAtt("update", mapping.Update);
         }
     }
 }
