01d19f97ba951e5203cca6fde683267003d62e5c
James Gregory <james@jagregory.com>
2009-07-19 17:22:37 +0100

    Implemented ISubclassInspector

--- src/FluentNHibernate/Conventions/Inspections/ISubclassInspector.cs
+++ src/FluentNHibernate/Conventions/Inspections/ISubclassInspector.cs
@@ -1,5 +1,27 @@
+using System;
+using System.Collections.Generic;
+using FluentNHibernate.MappingModel;
+
 namespace FluentNHibernate.Conventions.Inspections
 {
     public interface ISubclassInspector : IInspector
-    {}
+    {
+        bool Abstract { get; }
+        IEnumerable<IAnyInspector> Anys { get; }
+        IEnumerable<ICollectionInspector> Collections { get; }
+        object DiscriminatorValue { get; }
+        bool DynamicInsert { get; }
+        bool DynamicUpdate { get; }
+        string Extends { get; }
+        IEnumerable<IJoinInspector> Joins { get; }
+        Laziness LazyLoad { get; }
+        string Name { get; }
+        IEnumerable<IOneToOneInspector> OneToOnes { get; }
+        IEnumerable<IPropertyInspector> Properties { get; }
+        string Proxy { get; }
+        IEnumerable<IManyToOneInspector> References { get; }
+        bool SelectBeforeUpdate { get; }
+        IEnumerable<ISubclassInspector> Subclasses { get; }
+        Type Type { get; }
+    }
 }
