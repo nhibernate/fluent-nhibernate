0caa99d09dbaba8d5468c1011b0026bfa868c06e
James Gregory <james@jagregory.com>
2009-08-15 17:13:25 +0100

    Fixed ForeignKeyConvention to also do joined-subclass and joins

--- src/FluentNHibernate/Conventions/ForeignKeyConvention.cs
+++ src/FluentNHibernate/Conventions/ForeignKeyConvention.cs
@@ -7,7 +7,7 @@ using FluentNHibernate.Conventions.Inspections;
 namespace FluentNHibernate.Conventions
 {
     public abstract class ForeignKeyConvention
-        : IReferenceConvention, IHasManyConvention, IHasManyToManyConvention, IJoinedSubclassConvention
+        : IReferenceConvention, IHasManyConvention, IHasManyToManyConvention, IJoinedSubclassConvention, IJoinConvention
     {
         protected abstract string GetKeyName(PropertyInfo property, Type type);
 
@@ -39,8 +39,15 @@ namespace FluentNHibernate.Conventions
 
         public void Apply(IJoinedSubclassInstance instance)
         {
-            var columnName = GetKeyName(null, instance.Type);
+            var columnName = GetKeyName(null, instance.Type.BaseType);
             
+            instance.Key.Column(columnName);
+        }
+
+        public void Apply(IJoinInstance instance)
+        {
+            var columnName = GetKeyName(null, instance.EntityType);
+
             instance.Key.Column(columnName);
         }
     }
