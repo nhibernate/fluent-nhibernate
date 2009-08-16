64e9126ec1409ea90f843159856edc66d6a1bb21
James Gregory <james@jagregory.com>
2009-08-17 00:19:17 +0100

    Updated version numbers to 1.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 using System.Security;
 
 [assembly: AssemblyCompany("http://fluentnhibernate.org")]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 [assembly: AssemblyProduct("FluentNHibernate")]
-[assembly: AssemblyCopyright("Copyright 2008-2009 James Gregory and contributors (Paul Batum, Andrew Stewart, Chad Myers et al). All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright 2008-2009 James Gregory and contributors (Paul Batum, Andrew Stewart, Hudson Akridge, Stuart Childs et al). All rights reserved.")]
 		
