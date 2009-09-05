e1bc2527e8dcfa8cde8e519d912ee45681af2206
Paul Batum <paul.batum@gmail.com>
2009-09-05 11:49:59 +0800

    Added an ignored test that demonstrates how using an automapping overrides to modify the HibernateMapping doesn't work properly. Corresponding issue has been created in google code, #317.

---
+++ src/FluentNHibernate.Testing/AutoMapping/Overrides/HibernateMappingOverrides.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.MappingModel;
+using FluentNHibernate.Testing.Automapping;
+using NUnit.Framework;
+
+namespace FluentNHibernate.Testing.AutoMapping.Overrides
+{
+    [TestFixture]
+    public class HibernateMappingOverrides
+    {
+        [Test, Ignore]
+        public void CanOverrideDefaultLazy()
+        {
+            var model = AutoMap.Source(new StubTypeSource(new[] { typeof(Parent) }))
+               .Override<Parent>(o => o.HibernateMapping.Not.DefaultLazy());
+
+            model.CompileMappings();
+            HibernateMapping hibernateMapping = model.BuildMappings().First();
+
+            hibernateMapping.DefaultLazy.ShouldBeFalse();
+        }
+
+    }
+}
