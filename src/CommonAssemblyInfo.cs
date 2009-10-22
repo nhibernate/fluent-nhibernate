b158796243ea907fbca8d7ca497b1c6adf7d0944
Paul Batum <paul.batum@gmail.com>
2009-10-22 20:43:41 +1100

    Modified .gitignore to ignore the TestResult.xml file, and included changes to CommonAssemblyInfo.cs that occured via rake build.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 using System.Security;
 
-[assembly: AssemblyCompany("http://fluentnhibernate.org")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: AssemblyProduct("FluentNHibernate")]
 [assembly: AssemblyCopyright("Copyright 2008-2009 James Gregory and contributors (Paul Batum, Andrew Stewart, Hudson Akridge, Stuart Childs et al). All rights reserved.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("http://fluentnhibernate.org")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyProduct("FluentNHibernate")]
 		
