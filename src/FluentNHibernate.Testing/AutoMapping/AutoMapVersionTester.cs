cd39e3e1e0d8649487cc3228b3c42c184bc1c45a
Paul Batum <paul.batum@gmail.com>
2009-12-02 21:02:58 +1100

    Added tests provided by Jon Kruger for problem where byte[] version columns weren't getting mapped if the property was on a base class

--- src/FluentNHibernate.Testing/AutoMapping/AutoMapVersionTester.cs
+++ src/FluentNHibernate.Testing/AutoMapping/AutoMapVersionTester.cs
@@ -81,4 +81,71 @@ namespace FluentNHibernate.Testing.Automapping
         private class SubTarget : Target
         {}
     }
+
+    [TestFixture]
+    public class When_mapping_a_byte_array_version_property_and_the_version_property_is_on_a_base_class
+    {
+        private AutoMapVersion mapper;
+
+        [SetUp]
+        public void CreateMapper()
+        {
+            mapper = new AutoMapVersion();
+        }
+
+        [Test]
+        public void ShouldMapByteArray()
+        {
+            mapper.MapsProperty(typeof(BaseEntityClass).GetProperty("Version")).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldMapByteArrayAsBinaryBlob()
+        {
+            var mapping = new ClassMapping { Type = typeof(Target) };
+
+            mapper.Map(mapping, typeof(BaseEntityClass).GetProperty("Version"));
+
+            mapping.Version.Type.ShouldEqual(new TypeReference("BinaryBlob"));
+        }
+
+        [Test]
+        public void ShouldMapByteArrayAsTimestampSqlType()
+        {
+            var mapping = new ClassMapping { Type = typeof(Target) };
+
+            mapper.Map(mapping, typeof(BaseEntityClass).GetProperty("Version"));
+
+            mapping.Version.Columns.All(x => x.SqlType == "timestamp").ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldMapByteArrayAsNotNull()
+        {
+            var mapping = new ClassMapping { Type = typeof(Target) };
+
+            mapper.Map(mapping, typeof(BaseEntityClass).GetProperty("Version"));
+
+            mapping.Version.Columns.All(x => x.NotNull == true).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldMapByteArrayWithUnsavedValueOfNull()
+        {
+            var mapping = new ClassMapping { Type = typeof(Target) };
+
+            mapper.Map(mapping, typeof(BaseEntityClass).GetProperty("Version"));
+
+            mapping.Version.UnsavedValue.ShouldEqual(null);
+        }
+
+        private class Target : BaseEntityClass
+        {
+        }
+
+        private class BaseEntityClass
+        {
+            public byte[] Version { get; set; }
+        }
+    }
 }
