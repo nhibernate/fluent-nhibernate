8edee3673e85bece89fa72cf076a8fdde0e13004
James Gregory <james@jagregory.com>
2010-05-24 00:15:04 -0600

    Updated versions to 1.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("http://fluentnhibernate.org")]
 [assembly: AssemblyProduct("FluentNHibernate")]
-[assembly: AssemblyCopyright("Copyright 2008-2009 James Gregory and contributors (Paul Batum, Andrew Stewart, Hudson Akridge, Stuart Childs et al). All rights reserved.")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright 2008-2010 James Gregory and contributors (Paul Batum, Hudson Akridge et al). All rights reserved.")]
+[assembly: AssemblyVersion("1.1.0.0")]
 
 [assembly: AllowPartiallyTrustedCallers()]
 
