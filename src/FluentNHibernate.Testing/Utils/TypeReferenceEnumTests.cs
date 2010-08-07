72ecfe631ac6ddec857cdc3b2aba1ba17893b78e
Paul Batum <paul.batum@gmail.com>
2010-08-07 22:10:45 +1000

    Modified TypeReference.IsEnum to return true if its wrapping a GenericEnumMapper.

---
+++ src/FluentNHibernate.Testing/Utils/TypeReferenceEnumTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.MappingModel;
+using NUnit.Framework;
+using FluentNHibernate.Mapping;
+
+namespace FluentNHibernate.Testing.Utils
+{
+    [TestFixture]
+    public class TypeReferenceEnumTests
+    {
+        [Test]
+        public void IsEnumOnTypeReferenceToGenericEnumMapperShouldBeTrue()
+        {
+            var enumTypeReference = new TypeReference(typeof(GenericEnumMapper<TestEnum>));
+            enumTypeReference.IsEnum.ShouldBeTrue();
+        }
+
+        private enum TestEnum
+        {
+            Value1            
+        }
+
+        
+    }
+}
