2fc284fcb0f8a76be0f16c36e7b98100f42dbf5b
James Gregory <james@jagregory.com>
2012-04-27 10:43:08 +1000

    Switched to using bundler for gem management

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+source 'http://gemcutter.org'
+
+gem 'rake'
+gem 'albacore'
--- InstallGems.bat
+++ InstallGems.bat
@@ -2,10 +2,8 @@
 
 echo Setting up rake environment for building
 
-echo Installing Rake
-call gem install rake
+echo Installing Bundler
+gem install bundler
 
-call rake setup:ensure_gemcutter_source
-
-echo Installing Albacore v0.1.5 (newer builds bork it!)
-call gem install albacore -v 0.1.5
+echo Bundle Installing gems
+bundle install
