b8c0869f18f3432caf1e57b4f1fca123d6f1a543
Chermennov Gleb <thebitterend77@gmail.com>
2012-10-24 21:21:54 +0400

    applied changes. 1 MSpec test broken

--- src/FluentNHibernate.Testing/DomainModel/Mapping/CompositeIdentityPartTester.cs
+++ src/FluentNHibernate.Testing/DomainModel/Mapping/CompositeIdentityPartTester.cs
@@ -160,6 +160,25 @@ namespace FluentNHibernate.Testing.DomainModel.Mapping
                     .HasName("key-property");
         }
 
+        [Test]
+        public void CompositeIdWithSubclass()
+        {
+            new MappingTester<CompIdTarget>()
+                .SubClassMapping<ComIdSubclass>(s =>
+                {
+                    s.Table("subclasstable");
+                    s.KeyColumn("subclass_longId");
+                    s.KeyColumn("subclass_nullablelongId");
+                })
+                .ForMapping(c => c.CompositeId()
+                    .KeyProperty(x => x.LongId)
+                    .KeyProperty(x => x.NullableLongId))
+                .Element("class/joined-subclass/key/*[1]")
+                    .Exists().HasName("column").HasAttribute("name", "subclass_longId")
+                .RootElement.Element("class/joined-subclass/key/*[2]")
+                    .Exists().HasName("column").HasAttribute("name", "subclass_nullablelongId");
+        }
+
         public class CompIdTarget
         {
             public virtual long LongId { get; set; }
@@ -168,6 +187,11 @@ namespace FluentNHibernate.Testing.DomainModel.Mapping
             public virtual CompIdChild Child { get; set; }
             public virtual string DummyProp { get; set; }
             public virtual SomeEnum EnumProperty { get; set; }
+        }
+
+        public class ComIdSubclass : CompIdTarget
+        {
+        
         }
 
         public enum SomeEnum
