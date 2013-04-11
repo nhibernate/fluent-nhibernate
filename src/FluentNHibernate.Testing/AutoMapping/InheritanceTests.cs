ebed4c30467ac446a798c974730c3c6218bd2046
Chermennov Gleb <thebitterend77@gmail.com>
2013-04-12 01:55:10 +0400

    moved test classes around

--- src/FluentNHibernate.Testing/AutoMapping/InheritanceTests.cs
+++ src/FluentNHibernate.Testing/AutoMapping/InheritanceTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using FluentNHibernate.Automapping.TestFixtures.SuperTypes;
 using FluentNHibernate.Testing.Automapping;
 using NUnit.Framework;
 
@@ -22,34 +23,5 @@ namespace FluentNHibernate.Testing.AutoMapping
                 mapping.Element("//property[@name='SecondRate']/column").HasAttribute("name", "SecondRate");
             });
         }
-    }
-
-    public abstract class Base1
-    {
-        public virtual int Id { get; protected set; }
-        public abstract void Foo(int x);
-    }
-
-    public class Derived1: Base1
-    {
-        public virtual Decimal Rate { get; set; }
-        public override void Foo(int x)
-        {
-        }
-
-        public string GetSomething()
-        {
-            return Environment.NewLine;
-        }
-    }
-
-    public class SecondLevel: Derived1
-    {
-        public virtual Double SecondRate { get; set; }
-    }
-
-    public class ThirdLevel: SecondLevel
-    {
-        public virtual Boolean Flag { get; set; }
     }
 }
