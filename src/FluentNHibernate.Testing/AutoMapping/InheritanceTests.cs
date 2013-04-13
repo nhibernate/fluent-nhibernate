d5d9f91eba1387317bf4c53183f2f46051c718c8
Chermennov Gleb <thebitterend77@gmail.com>
2013-04-13 09:38:22 +0400

    more tests for automapping inheritance hierarchies #113

--- src/FluentNHibernate.Testing/AutoMapping/InheritanceTests.cs
+++ src/FluentNHibernate.Testing/AutoMapping/InheritanceTests.cs
@@ -18,9 +18,56 @@ namespace FluentNHibernate.Testing.AutoMapping
 
             Test<ThirdLevel>(mapping =>
             {
-                mapping.Element("//id").HasAttribute("name", "Id");
                 mapping.Element("//property[@name='Rate']/column").HasAttribute("name", "Rate");
                 mapping.Element("//property[@name='SecondRate']/column").HasAttribute("name", "SecondRate");
+            });
+        }
+
+        [Test]
+        public void AutoMapInheritedIdProperty()
+        {
+            Model<SecondLevel>();
+
+            Test<SecondLevel>(mapping => mapping.Element("//id").HasAttribute("name", "Id"));
+        }
+
+        [Test]
+        public void AutoMapManyToOne()
+        {
+            Model<FourthLevel>();
+
+            Test<FourthLevel>(mapping => mapping.Element("//many-to-one").HasAttribute("name", "One").
+                Element("//many-to-one/column").HasAttribute("name", "One_id"));
+        }
+
+        [Test]
+        public void AutoMapManyToMany()
+        {
+            Model<FourthLevel>();
+
+            Test<FourthLevel>(mapping => 
+            { 
+                mapping.Element("//many-to-many/column").HasAttribute("name", "ManyToMany_id");
+            });
+        }
+
+        [Test]
+        public void AutoMapEnum()
+        {
+            Model<FourthLevel>(model => model.Override<FourthLevel>(map => map.Map(x => x.publisherType)));
+
+            Test<FourthLevel > (mapping => mapping.Element("//property[@name='publisherType']").Exists());
+        }
+
+        [Test]
+        public void AutoMapVersion()
+        {
+            Model<ThirdLevel>();
+
+            Test<ThirdLevel>(mapping =>
+            {
+                mapping.Element("//version").HasAttribute("name", "Version");
+                mapping.Element("//version/column").HasAttribute("name", "Version");
             });
         }
     }
