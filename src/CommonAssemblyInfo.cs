671bc3fcc36530ceceb0e7f57ba2168797abe0f5
Chermennov Gleb <thebitterend77@gmail.com>
2014-08-31 15:30:15 +0400

    brought back Sqlite configuration for tests

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,7 +1,9 @@
 using System.Security;
 using System.Reflection;
 using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("c336bcfedf38354c0e4d02a87a80bf2acb549b42")]
 [assembly: AssemblyCompany("http://fluentnhibernate.org")]
 [assembly: AssemblyProduct("FluentNHibernate")]
 [assembly: AssemblyCopyright("Copyright 2008-2014 James Gregory and contributors (Paul Batum, Hudson Akridge et al). All rights reserved.")]
 [assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
