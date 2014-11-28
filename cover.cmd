15e68eec5abf550d4b7e6294852a60b7c379a4e9
Chermennov Gleb <thebitterend77@gmail.com>
2014-11-29 01:56:47 +0300

    respect NUnit framework version

--- cover.cmd
+++ cover.cmd
@@ -3,17 +3,18 @@
 echo ##teamcity[dotNetCoverage]
 
 set initial_path=%CD%
-
 cd coverage/settings
 
-echo Running unit tests with coverage
 echo Searching for dotCover executable...
 
-SET mspec_path=..\..\src\packages\Machine.Specifications.0.5.15\tools\mspec%2.exe
-SET nunit_path=..\..\src\packages\NUnit.2.5.7.10213\Tools\nunit-console-x86.exe
+set mspec_path=..\..\src\packages\Machine.Specifications.0.5.15\tools\mspec%2.exe
+set nunit_path=..\..\src\packages\NUnit.2.5.7.10213\Tools\nunit-console-x86.exe
 
 echo Using MSpec from "%mspec_path%"
 echo Using NUnit from "%nunit_path%"
+
+set runtime_version=%3
+echo Running NUnit tests with %runtime_version%
 
 set dotcover_path=%1
 echo Using dotCover from %dotcover_path%
@@ -21,10 +22,12 @@ echo Using dotCover from %dotcover_path%
 IF exist ..\results/nul ( echo Coverage results folder exists) ELSE ( mkdir ..\results && echo Coverage results folder created)
 
 echo Running tests with coverage...
-call "%dotcover_path%" cover nunit-coverage.xml /TargetExecutable=%nunit_path% /TargetArguments=%CD%/../../src/FluentNHibernate.Testing/bin/Release/FluentNHibernate.Testing.dll /framework:%3 /xml:../results/NUnitResult.xml
+call "%dotcover_path%" cover nunit-coverage.xml /TargetExecutable=%nunit_path% /TargetArguments="%CD%/../../src/FluentNHibernate.Testing/bin/Release/FluentNHibernate.Testing.dll /framework:%runtime_version% /xml:../results/NUnitOutput.xml"
 call "%dotcover_path%" cover mspec-coverage.xml /TargetExecutable=%mspec_path% /TargetArguments="FluentNHibernate.Specs.dll"
-echo Merging reports...
+echo Merging coverage snapshots...
 call "%dotcover_path%" merge /Source="..\results\NUnitOutput.xml;..\results\MSpecOutput.xml" /Output="..\results\merged.xml"
+echo Creating readable report...
+call "%dotcover_path%" report /Source="..\results\merged.xml" /Output="..\results\report.xml" /ReportType=XML
 
 echo Reporting coverage results to TeamCity...
 
