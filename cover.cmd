8477ae042619230b02d4b1982c80c3693ca0f7ae
Chermennov Gleb <thebitterend77@gmail.com>
2014-11-29 01:05:36 +0300

    already better

--- cover.cmd
+++ cover.cmd
@@ -1,4 +1,8 @@
-echo '##teamcity[dotNetCoverage]'
+@echo off
+
+echo ##teamcity[dotNetCoverage]
+
+set initial_path=%CD%
 
 cd coverage/settings
 
@@ -8,19 +12,25 @@ echo Searching for dotCover executable...
 SET mspecpath=..\..\src\packages\Machine.Specifications.0.5.15\tools\mspec%2.exe
 SET nunit_path=..\..\src\packages\NUnit.2.5.7.10213\Tools\nunit-console-x86.exe
 
-echo MSpec is here "%mspecpath%"
-echo NUnit is here "%nunit_path%"
+echo Using MSpec from "%mspecpath%"
+echo Using NUnit from "%nunit_path%"
 
 set dotcover_path=%1
 echo Using dotCover from %dotcover_path%
 
-echo Running coverage...
-start "%dotcover_path%" cover nunit-coverage.xml /TargetExecutable=%nunit_path% /TargetArguments=%CD%+"/../../src/FluentNHibernate.Testing/bin/Release/FluentNHibernate.Testing.dll /framework:" + %3 + " /xml:../results/NUnitResult.xml"
-start "%dotcover_path%" cover mspec-coverage.xml /TargetExecutable=%mspec_path% /TargetArguments="FluentNHibernate.Specs.dll"
+IF exist ..\results/nul ( echo Coverage results folder exists) ELSE ( mkdir ..\results && echo Coverage results folder created)
+
+echo Running tests with coverage...
+start "" "%dotcover_path%" cover nunit-coverage.xml /TargetExecutable=%nunit_path% /TargetArguments=%CD%/../../src/FluentNHibernate.Testing/bin/Release/FluentNHibernate.Testing.dll /framework:%3 /xml:../results/NUnitResult.xml"
+start "" "%dotcover_path%" cover mspec-coverage.xml /TargetExecutable=%mspec_path% /TargetArguments="FluentNHibernate.Specs.dll"
 echo Merging reports...
-start "%dotcover_path%" merge /Source="..\\results\\NUnitOutput.xml;..\\results\\MSpecOutput.xml" /Output="..\\results\\merged.xml" 
+start "" "%dotcover_path%" merge /Source="..\\results\\NUnitOutput.xml;..\\results\\MSpecOutput.xml" /Output="..\\results\\merged.xml"
 
-echo Done, reporting to TeamCity...
+echo Reporting coverage results to TeamCity...
 
-echo "##teamcity[importData type='dotNetCoverage' tool='dotcover' path='coverage/results/NUnitOutput.xml']"
-echo "##teamcity[importData type='dotNetCoverage' tool='dotcover' path='coverage/results/MSpecOutput.xml']"
+echo ##teamcity[importData type='dotNetCoverage' tool='dotcover' path='coverage/results/NUnitOutput.xml']
+echo ##teamcity[importData type='dotNetCoverage' tool='dotcover' path='coverage/results/MSpecOutput.xml']
+
+cd %initial_path%
+
+echo Done.
