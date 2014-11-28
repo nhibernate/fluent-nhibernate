8fa4b43e6e77b920084a427657cf76b377481085
Chermennov Gleb <thebitterend77@gmail.com>
2014-11-28 10:51:54 +0300

    make coverage script respect config.json values, call it from rake

--- cover.sh
+++ cover.sh
@@ -7,12 +7,15 @@ cd coverage/settings
 echo Running unit tests with coverage
 echo Searching for dotCover executable...
 
-TC_DOTCOVER_PATH=${1:-"/c/TeamCity/buildAgent/tools/dotCover/dotCover.exe"}
-echo Using dotCover from ${TC_DOTCOVER_PATH}
+MSPEC_PATH="../../src/packages/Machine.Specifications.0.5.15/tools/mspec${2}.exe"
+NUNIT_PATH="../../src/packages/NUnit.2.5.7.10213/Tools/nunit-console-x86.exe"
+
+DOTCOVER_PATH=${1:-"/c/TeamCity/buildAgent/tools/dotCover/dotCover.exe"}
+echo Using dotCover from ${DOTCOVER_PATH}
 
 echo Running coverage...
-${TC_DOTCOVER_PATH} cover nunit-coverage.xml
-${TC_DOTCOVER_PATH} cover mspec-coverage.xml
+${DOTCOVER_PATH} cover nunit-coverage.xml /TargetExecutable=${NUNIT_PATH} /TargetArguments="FluentNHibernate.Testing.dll /framework:${3} /xml:../results/NUnitResult.xml"
+${DOTCOVER_PATH} cover mspec-coverage.xml /TargetExecutable=${MSPEC_PATH} /TargetArguments="FluentNHibernate.Specs.dll"
 
 echo Done, reporting to TeamCity...
 
