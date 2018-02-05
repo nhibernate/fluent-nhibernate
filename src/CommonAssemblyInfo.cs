76a81f018ff4ffdeac70cfa6fdd1782e78179fec
Jorge Rodríguez Galán <jrogalan@gmail.com>
2018-02-05 20:20:29 +0100

    fix(project): minor fix for year and unused package

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
-[assembly: AssemblyCopyright("Copyright 2008-2017 James Gregory and contributors (Paul Batum, Hudson Akridge, Gleb Chermennov, Jorge Rodríguez Galán). All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright 2008-2018 James Gregory and contributors (Paul Batum, Hudson Akridge, Gleb Chermennov, Jorge Rodríguez Galán). All rights reserved.")]
