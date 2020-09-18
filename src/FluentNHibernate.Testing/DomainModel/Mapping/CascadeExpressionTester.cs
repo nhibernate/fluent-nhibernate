9d52a2e41c6954ba1c81f8f8875c09e178bbbd79
Alex Zaytsev <hazzik@gmail.com>
2020-09-18 15:15:40 +1200

    Add test

--- src/FluentNHibernate.Testing/DomainModel/Mapping/CascadeExpressionTester.cs
+++ src/FluentNHibernate.Testing/DomainModel/Mapping/CascadeExpressionTester.cs
@@ -93,5 +93,11 @@ namespace FluentNHibernate.Testing.DomainModel.Mapping
         {
             A_call_to(_cascade.Persist).should_set_the_cascade_value_to("save-update, persist");
         }
+
+        [Test]
+        public void AllDeleteOrphan_should_add_the_correct_cascade_attribute_to_the_parent_part()
+        {
+            A_call_to(_cascade.AllDeleteOrphan).should_set_the_cascade_value_to("all-delete-orphan");
+        }
     }
 }
