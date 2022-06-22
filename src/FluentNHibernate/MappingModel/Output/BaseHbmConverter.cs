84ecbf48a1615a1d2a9307ae42b6e7bbeeef3349
Joel Aelwyn <joel.aelwyn@tylertech.com>
2022-06-22 14:51:04 -0400

    Add shared parent for Fluent→HBM converters

---
+++ src/FluentNHibernate/MappingModel/Output/BaseHbmConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using FluentNHibernate.Visitors;
+
+namespace FluentNHibernate.MappingModel.Output
+{
+    public abstract class BaseHbmConverter<F, H> : NullMappingModelVisitor, IHbmConverter<F, H>
+        where F: IMapping
+    {
+        private readonly IHbmConverterServiceLocator serviceLocator;
+
+        protected BaseHbmConverter(IHbmConverterServiceLocator serviceLocator)
+        {
+            this.serviceLocator = serviceLocator;
+        }
+
+        public abstract H Convert(F mapping);
+
+        protected HSub ConvertFluentSubobjectToHibernateNative<FSub, HSub>(FSub fluentMapping)
+            where FSub : IMapping
+        {
+            var converter = serviceLocator.GetConverter<FSub, HSub>();
+            return converter.Convert(fluentMapping);
+        }
+
+        protected void AddToNullableArray<T>(ref T[] nullableArray, T item)
+        {
+            if (nullableArray == null)
+                nullableArray = new T[0];
+            Array.Resize(ref nullableArray, nullableArray.Length + 1);
+            nullableArray[nullableArray.Length - 1] = item;
+        }
+    }
+}
