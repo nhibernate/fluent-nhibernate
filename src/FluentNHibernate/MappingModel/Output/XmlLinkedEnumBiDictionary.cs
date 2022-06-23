5c4517c38ef3136303f16fb9528f7aedada81bcc
Joel Aelwyn <joel.aelwyn@tylertech.com>
2022-06-23 17:38:21 -0400

    Add a bi-dictionary for XML string←→enum lookups

---
+++ src/FluentNHibernate/MappingModel/Output/XmlLinkedEnumBiDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using FluentNHibernate.Utils;
+
+namespace FluentNHibernate.MappingModel.Output
+{
+    public class XmlLinkedEnumBiDictionary<E>
+        where E : Enum
+    {
+        private readonly IReadOnlyDictionary<string, E> stringToMember;
+        private readonly IReadOnlyDictionary<E, string> memberToString;
+
+        public XmlLinkedEnumBiDictionary()
+        {
+            stringToMember = Enum.GetValues(typeof(E))
+                .Cast<E>()
+                .ToDictionary(e => e.GetCustomAttribute<XmlEnumAttribute>().Name);
+            memberToString = stringToMember.ToDictionary(pair => pair.Value, pair => pair.Key);
+        }
+
+        public string this [E enumValue]
+        {
+            get { return memberToString[enumValue]; }
+        }
+
+        public E this [string xmlString]
+        {
+            get { return stringToMember[xmlString]; }
+        }
+    }
+}
