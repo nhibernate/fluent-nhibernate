dc29d81c6fc17637713e1a4d2a564e93e5710f28
Joel Aelwyn <joel.aelwyn@tylertech.com>
2022-06-29 17:12:06 -0400

    Remove stray commented code

--- src/FluentNHibernate/MappingModel/Output/HbmIdentityBasedConverter.cs
+++ src/FluentNHibernate/MappingModel/Output/HbmIdentityBasedConverter.cs
@@ -6,10 +6,6 @@ namespace FluentNHibernate.MappingModel.Output
 {
     public class HbmIdentityBasedConverter : HbmConverterBase<IIdentityMapping, object>
     {
-        /*
-        private readonly IXmlWriterServiceLocator serviceLocator;
-        private XmlDocument document;
-        */
         private object hbm;
 
         public HbmIdentityBasedConverter(IHbmConverterServiceLocator serviceLocator) : base(serviceLocator)
@@ -18,24 +14,10 @@ namespace FluentNHibernate.MappingModel.Output
 
         public override object Convert(IIdentityMapping identityMapping)
         {
-            hbm = new object(); // FIXME: Dummy to avoid build failure, remove!
             identityMapping.AcceptVisitor(this);
             return hbm;
         }
 
-        /*
-        public override void ProcessId(IdMapping mapping)
-        {
-            var writer = serviceLocator.GetWriter<IdMapping>();
-            document = writer.Write(mapping);
-        }
-
-        public override void ProcessCompositeId(CompositeIdMapping idMapping)
-        {
-            var writer = serviceLocator.GetWriter<CompositeIdMapping>();
-            document = writer.Write(idMapping);
-        }
-        */
         public override void ProcessId(IdMapping idMapping)
         {
             hbm = ConvertFluentSubobjectToHibernateNative<IdMapping, HbmId>(idMapping);
