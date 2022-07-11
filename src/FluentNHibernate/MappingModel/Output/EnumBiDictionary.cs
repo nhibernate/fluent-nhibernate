9b63d3946a09e6cf025a7aa6ea04ef6eba9c087c
Joel Aelwyn <joel.aelwyn@tylertech.com>
2022-07-11 19:16:38 -0400

    Refactor to extract reusable EnumBiDictionary

---
+++ src/FluentNHibernate/MappingModel/Output/EnumBiDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentNHibernate.MappingModel.Output
+{
+    public class EnumBiDictionary<E, V>
+        where E : Enum
+    {
+        private readonly IReadOnlyDictionary<V, E> valueToMember;
+        private readonly IReadOnlyDictionary<E, V> memberToValue;
+
+        public EnumBiDictionary(Func<E, V> enumToValueMapper)
+        {
+            valueToMember = Enum.GetValues(typeof(E))
+                .Cast<E>()
+                .ToDictionary(enumToValueMapper);
+            memberToValue = valueToMember.ToDictionary(pair => pair.Value, pair => pair.Key);
+        }
+
+        public EnumBiDictionary(IReadOnlyDictionary<E, V> enumToValueDict) : this(e => enumToValueDict[e])
+        {
+        }
+
+        public V this [E enumMember]
+        {
+            get { return memberToValue[enumMember]; }
+        }
+
+        public E this [V value]
+        {
+            get { return valueToMember[value]; }
+        }
+    }
+}
--- src/FluentNHibernate/MappingModel/Output/XmlLinkedEnumBiDictionary.cs
+++ src/FluentNHibernate/MappingModel/Output/XmlLinkedEnumBiDictionary.cs
@@ -1,33 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Xml.Serialization;
 using FluentNHibernate.Utils;
 
 namespace FluentNHibernate.MappingModel.Output
 {
-    public class XmlLinkedEnumBiDictionary<E>
+    public class XmlLinkedEnumBiDictionary<E> : EnumBiDictionary<E, string>
         where E : Enum
     {
-        private readonly IReadOnlyDictionary<string, E> stringToMember;
-        private readonly IReadOnlyDictionary<E, string> memberToString;
-
-        public XmlLinkedEnumBiDictionary()
+        public XmlLinkedEnumBiDictionary() : base(e => e.GetCustomAttribute<XmlEnumAttribute>().Name)
         {
-            stringToMember = Enum.GetValues(typeof(E))
-                .Cast<E>()
-                .ToDictionary(e => e.GetCustomAttribute<XmlEnumAttribute>().Name);
-            memberToString = stringToMember.ToDictionary(pair => pair.Value, pair => pair.Key);
-        }
-
-        public string this [E enumValue]
-        {
-            get { return memberToString[enumValue]; }
-        }
-
-        public E this [string xmlString]
-        {
-            get { return stringToMember[xmlString]; }
         }
     }
 }
